a7b413f412532f10ce17360c80ece9900d81bbea
209347 <209347@edu.p.lodz.pl>
2019-06-04 23:02:29 +0200

    Added RepeatRule

--- mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -1,15 +1,19 @@
 package edu.iis.mto.multithread;
 
+import org.junit.Rule;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class BetterRadarTest {
 
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
     @Test
+    @RepeatRule.Repeat(times = 100)
     public void patriotBatteryShouldCallLaunchPatriotOnce() {
         PatriotBattery patriotBatteryMock = mock(PatriotBattery.class);
         BetterRadar betterRadar = new BetterRadar(patriotBatteryMock, new BatteryExecutor(true), 1);
